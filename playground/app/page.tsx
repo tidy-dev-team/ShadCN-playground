e8957366857862e32aa7c34b052b0f5143a18b23
Adir <slutzkis@gmail.com>
2025-06-30 12:22:53 +0300

    Update page.tsx

--- playground/app/page.tsx
+++ playground/app/page.tsx
@@ -73,14 +73,14 @@ export default function MyCustomDialog() {
         </DialogFooter>
 
         <DialogClose asChild>
-          <Button
+          {/* <Button
             variant="ghost"
             size="icon"
             className="absolute right-6 top-6 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground"
           >
             <X className="h-4 w-4" />
             <span className="sr-only">Close</span>
-          </Button>
+          </Button> */}
         </DialogClose>
       </DialogContent>
     </Dialog>
